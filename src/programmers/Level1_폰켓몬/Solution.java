1050b260e729167e2d3f6c497c5bcd42bace26e8
sskim91 <tjdtjq91@gmail.com>
2024-09-03 22:44:57 +0900

    add programmers

---
+++ src/programmers/Level1_폰켓몬/Solution.java
@@ -0,0 +1,8 @@
+package programmers.Level1_폰켓몬;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        return answer;
+    }
+}
---
+++ src/programmers/Level2_짝지어제거하기/Solution.java
@@ -0,0 +1,12 @@
+package programmers.Level2_짝지어제거하기;
+
+class Solution {
+    public int solution(String s) {
+        int answer = -1;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+}
