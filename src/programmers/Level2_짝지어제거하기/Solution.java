5a5aef75bce75624106fb0683a09d84be2985c64
sskim91 <tjdtjq91@gmail.com>
2025-02-10 23:40:51 +0900

    Add: 짝지어 제거하기

--- src/programmers/Level2_짝지어제거하기/Solution.java
+++ src/programmers/Level2_짝지어제거하기/Solution.java
@@ -1,12 +1,20 @@
 package programmers.Level2_짝지어제거하기;
 
+import java.util.Stack;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12973
 class Solution {
     public int solution(String s) {
-        int answer = -1;
-
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        return answer;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            final char c = s.charAt(i);
+            // 스택이 비어 있지 않고, 현재 문자와 스택의 맨 위 문자가 같으면
+            if (!stack.isEmpty() && stack.peek() == c) {
+                stack.pop();    //스택의 맨 위 문자 제거
+            } else {
+                stack.push(c);  //스택에 현재 문자 추가
+            }
+        }
+        return stack.isEmpty() ? 1 : 0; //스택이 비어 있으면 1, 아니면 0 반환
     }
 }
